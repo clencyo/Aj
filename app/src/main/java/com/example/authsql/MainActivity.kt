75876b5ed3e721cdebcbb65cc2c6f22442df6173
clencyo <audiajepkirui123@gmail.com>
2023-04-19 10:21:19 +0300

    first commit

--- app/src/main/java/com/example/authsql/MainActivity.kt
+++ app/src/main/java/com/example/authsql/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : AppCompatActivity() {
         var db = openOrCreateDatabase("AUDIAdb", MODE_PRIVATE, null)
 
         //creating table
-        db.execSQL("CREATE TABLE IF NOT EXISTS users(jina VARCHAR, arafa VARCHAR, kitambulisho INTAGER)")
+        db.execSQL("CREATE TABLE IF NOT EXISTS users(jinamoja VARCHAR, jinambili VACHAR, arafa VARCHAR, nambari VACHAR)")
 
         loginbutton.setOnClickListener {
             //Receive data from the user
@@ -48,7 +48,7 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "Cannot Submit empty Field", Toast.LENGTH_SHORT).show()
             }else{
                 //Proceed to save your data into the db
-                db.execSQL("INSERT INTO users VALUES('"+firsname+"','"+email+"','"+secname+"','"+password+"')")
+                db.execSQL("INSERT INTO users VALUES("'+firsname+"',"'+email+'","'+secname+'","'+password+'")")
 
                 //Toast a success message
 
--- app/src/main/java/com/example/authsql/login_activity.kt
+++ app/src/main/java/com/example/authsql/login_activity.kt
@@ -1,11 +1,37 @@
 package com.example.authsql
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class login_activity : AppCompatActivity() {
+
+    lateinit var email_login: EditText
+    lateinit var password_login: EditText
+    lateinit var login_login: EditText
+    lateinit var create_button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        email_login = findViewById(R.id.emailedt)
+        password_login = findViewById(R.id.passwordedt)
+        login_login = findViewById(R.id.btnforlogin)
+        create_button =findViewById(R.id.btnregister)
+
+        login_login.setOnClickListener {
+            val gotoCreateAcount = Intent(this,MainActivity::class.java)
+            startActivity(gotoCreateAcount)
+        }
+
+        create_button.setOnClickListener {
+            val gotoLogin = Intent(this,MainActivity::class.java)
+            startActivity(gotoLogin)
+        }
+
+
+
     }
 }
