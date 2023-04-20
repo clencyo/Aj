4e17976c46bf47c54ea3368cd0ba9550709e941b
clencyo <audiajepkirui123@gmail.com>
2023-04-20 10:45:11 +0300

    logoutactivity

--- app/src/main/java/com/example/authsql/DashboardActivity.kt
+++ app/src/main/java/com/example/authsql/DashboardActivity.kt
@@ -1,11 +1,24 @@
 package com.example.authsql
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class DashboardActivity : AppCompatActivity() {
+
+    lateinit var logoutbutton:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        logoutbutton = findViewById(R.id.btnlogout)
+
+        logoutbutton.setOnClickListener {
+
+            //navigating a user from one activity to the other
+            var gotologout = Intent(this, MainActivity::class.java)
+            startActivity(gotologout)
+        }
     }
 }
